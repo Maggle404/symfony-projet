0b6d24209c5808b5059c4a95fab318139dd0f01a
nicolasdraperi <nicolas.draperi2@gmail.com>
2024-01-05 12:30:08 +0100

    Login base page created

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class UserController extends AbstractController
-{
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+class UserController extends AbstractController{
+
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authdUtils): Response{
+    $error = $authdUtils->getLastAuthenticationError();
+    $last_Username = $authdUtils->getLastUsername();
+
+    return $this->render('user/index.html.twig', ['error' => $error, 'last_username' => $last_Username]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout($security): Response{
+    $security->logout(false);
+    return $this->redirectToRoute('app_login');
     }
 }
